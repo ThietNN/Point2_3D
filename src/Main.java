1e1053d08845944334c937e74567633a7556f747
ThietNN <nguyenthiet1225@gmail.com>
2022-02-14 14:11:10 +0700

    new pj

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Point2D p2d = new Point2D();
+        System.out.println(p2d);
+
+        Point3D p3d = new Point3D();
+        p3d.setXYZ(1,2,3);
+        System.out.println(p3d);
+    }
+}
